5d1a0aa4ac008be437fb1123a93f458fdfaf0568
wi1dcode <magosdk@icloud.com>
2022-07-12 11:37:50 +0200

    react app & bootstrap card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import users from './users.json'
 import Card from './components/Card'
+import "./index.css"
 
 class App extends React.Component {
   render() {    
     return (
-      <ul>
+      <ul className='row mt-1 gy-5 d-flex justify-content-center'>
       {
           users.map((user, key)=>(
             <Card user={user} key={key}/>
+          
           ))
       }
   </ul>
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,22 +4,23 @@ class Card extends React.Component {
   render() {
     console.log(this);
     return (
-      // <li>{this.props.user.name}</li>  
-        <div className="card mb-3" style={{maxWidth: 540}}>
+        <div className="card me-5" style={{maxWidth: 540}}>
           <div className="row g-0">
             <div className="col-md-4">
-              <img src={this.props.user.picture} className="img-fluid rounded-start"/>
+              <img src={this.props.user.picture}  className="img-fluid rounded-start" />
             </div>
             <div className="col-md-8">
               <div className="card-body">
                 <h5 className="card-title">{this.props.user.name}</h5>
                 <p className="card-text">{this.props.user.about}</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                <p className="card-text">{this.props.user.phone} <br />{this.props.user.email}</p>
+                <p className="card-text fs-6">{this.props.user.address}</p>
+                <p className="card-text"><small class="text-muted">Registered: {this.props.user.registered}</small></p>
               </div>
             </div>
           </div>
-        </div>
-      )
+        </div>  
+    )
   }
 }
 
